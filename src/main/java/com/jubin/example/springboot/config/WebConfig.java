92a272ce26f11729eea7787935f1079a135254d0
wnqlsdl63 <wnqlsdl63@naver.com>
2021-05-16 17:54:00 +0900

    Add Google Oauth2 Login

---
+++ src/main/java/com/jubin/example/springboot/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.jubin.example.springboot.config;
+
+import com.jubin.example.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/jubin/example/springboot/web/IndexController.java
+++ src/main/java/com/jubin/example/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jubin.example.springboot.web;
 
+import com.jubin.example.springboot.config.auth.dto.SessionUser;
 import com.jubin.example.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("postsList", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
